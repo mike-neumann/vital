772f77758c0444156ef426176e45a295aa956f72
xRa1ny <xra1nygames@gmail.com>
2024-07-17 16:32:53 +0200

    fixed spring not picking up the "application.properties" in built jar ROOT directory

--- vital-core/src/main/java/me/xra1ny/vital/Vital.java
+++ vital-core/src/main/java/me/xra1ny/vital/Vital.java
@@ -7,7 +7,11 @@ import me.xra1ny.vital.spring.VitalBungeecordConfiguration;
 import me.xra1ny.vital.spring.VitalSpigotConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+
+import java.util.Properties;
 
 /**
  * The main instance of the Vital-Framework.
@@ -25,18 +29,52 @@ public class Vital {
      */
     @SneakyThrows
     public static void run(Class<?> plugin, String pluginName) {
-        Thread.currentThread().setContextClassLoader(plugin.getClassLoader());
+        final ClassLoader pluginClassLoader = plugin.getClassLoader();
+        // needed or else spring startup fails
+        Thread.currentThread().setContextClassLoader(pluginClassLoader);
 
+        final ResourceLoader loader = new DefaultResourceLoader(pluginClassLoader);
         final SpringApplicationBuilder builder = new SpringApplicationBuilder();
         final Class<?> pluginConfiguration = Class.forName(plugin.getPackageName() + ".PluginConfiguration");
         final Class<?>[] sources = {pluginConfiguration, VitalSpigotConfiguration.class, VitalBungeecordConfiguration.class};
 
+        System.setProperty("plugin.name", pluginName);
+        System.setProperty("plugin.main", plugin.getName());
+
+        try {
+            final Properties properties = new Properties();
+
+            properties.load(pluginClassLoader.getResourceAsStream("application.properties"));
+
+            properties.forEach((key, value) -> {
+                System.setProperty(key.toString(), value.toString());
+                System.out.println("setting: " + key + "; to: " + value);
+            });
+        } catch (Exception ignored) {}
+
         context = builder.sources(sources)
-                .resourceLoader(new DefaultResourceLoader(plugin.getClassLoader()))
-                .logStartupInfo(false)
+                .initializers(applicationContext -> {
+                    applicationContext.setClassLoader(pluginClassLoader);
+                    applicationContext.setEnvironment(new StandardEnvironment());
+                })
+                .resourceLoader(loader)
                 .banner(new VitalBanner())
-                .properties("plugin.name=" + pluginName)
-                .properties("plugin.main=" + plugin.getName())
+                .logStartupInfo(false)
                 .run();
     }
+//    public static void run(Class<?> plugin, String pluginName) {
+//        Thread.currentThread().setContextClassLoader(plugin.getClassLoader());
+//
+//        final SpringApplicationBuilder builder = new SpringApplicationBuilder();
+//        final Class<?> pluginConfiguration = Class.forName(plugin.getPackageName() + ".PluginConfiguration");
+//        final Class<?>[] sources = {pluginConfiguration, VitalSpigotConfiguration.class, VitalBungeecordConfiguration.class};
+//
+//        context = builder.sources(sources)
+//                .resourceLoader(new DefaultResourceLoader(plugin.getClassLoader()))
+//                .logStartupInfo(false)
+//                .banner(new VitalBanner())
+//                .properties("plugin.name=" + pluginName)
+//                .properties("plugin.main=" + plugin.getName())
+//                .run();
+//    }
 }
