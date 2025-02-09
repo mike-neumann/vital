8d6aa9da2248a5aaa43c570c2bed5bd030e0c558
xRa1ny <mikeneumann2302@gmail.com>
2025-02-09 10:08:59 +0100

    fixed "already initialized" error on plugin startup

--- vital-core-processor/src/main/resources/Main.java
+++ vital-core-processor/src/main/resources/Main.java
@@ -1,6 +1,11 @@
 package {packageName};
 
+import me.vitalframework.Vital;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+// we want spring boot to scan every component but our main class, since that is instatiated by our minecraft server runtime
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Vital.Info.class))
 @SpringBootApplication(scanBasePackages = {"me.vitalframework", {scans}})
 public class PluginConfiguration {}
