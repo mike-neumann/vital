aec76b0012b6010c889c4006c3e28c53331e5884
xRa1ny <mikeneumann2302@gmail.com>
2025-02-16 11:44:11 +0100

    small refactorings regarding gradle plugin

--- vital-gradle-plugin/src/main/kotlin/me/vitalframework/VitalGradlePlugin.kt
+++ vital-gradle-plugin/src/main/kotlin/me/vitalframework/VitalGradlePlugin.kt
@@ -4,58 +4,53 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class VitalGradlePlugin : Plugin<Project> {
-    private lateinit var target: Project
-
     override fun apply(target: Project) {
-        this.target = target
-
         target.afterEvaluate {
-            requirePlugin(SHADOW_PLUGIN_ID, SHADOW_PLUGIN_EXAMPLE_VERSION)
-            target.dependencies.add("implementation", "$VITAL_CORE_DEPENDENCY:1.0")
+            requirePlugin(it, SHADOW_PLUGIN_ID, SHADOW_PLUGIN_EXAMPLE_VERSION)
+            target.dependencies.add("implementation", "$DEPENDENCY_VITAL_CORE:1.0")
 
             try {
                 // attempt to register annotation processor for kotlin
-                target.dependencies.add("kapt", "$VITAL_CORE_PROCESSOR_DEPENDENCY:1.0")
+                target.dependencies.add("kapt", "$DEPENDENCY_VITAL_CORE_PROCESSOR:1.0")
             } catch (e: Exception) {
                 // kapt not found, register default
-                target.dependencies.add("annotationProcessor", "$VITAL_CORE_PROCESSOR_DEPENDENCY:1.0")
+                target.dependencies.add("annotationProcessor", "$DEPENDENCY_VITAL_CORE_PROCESSOR:1.0")
             }
-            // if vital-commands is used, also add processor for module
-            if (hasDependency(VITAL_COMMANDS_DEPENDENCY)) {
+
+            if (hasDependency(it, DEPENDENCY_VITAL_COMMANDS)) {
+                // when vital-commands is used, we also want to use vital-commands-processor
                 try {
                     // attempt to register annotation processor for kotlin
-                    target.dependencies.add("kapt", "$VITAL_COMMANDS_PROCESSOR_DEPENDENCY:1.0")
+                    target.dependencies.add("kapt", "$DEPENDENCY_VITAL_COMMANDS_PROCESSOR:1.0")
                 } catch (e: Exception) {
                     // kapt not found, register default
-                    target.dependencies.add("annotationProcessor", "$VITAL_COMMANDS_PROCESSOR_DEPENDENCY:1.0")
+                    target.dependencies.add("annotationProcessor", "$DEPENDENCY_VITAL_COMMANDS_PROCESSOR:1.0")
                 }
             }
 
-            target.tasks.named("build") {
-                it.dependsOn(target.tasks.named("shadowJar"))
-            }
+            target.tasks.named("build") { it.dependsOn(target.tasks.named("shadowJar")) }
         }
     }
 
-    private fun requirePlugin(id: String, exampleVersion: String) {
+    private fun requirePlugin(project: Project, id: String, exampleVersion: String) {
         try {
             // this will fail when plugins are not detected on classpath
-            target.plugins.apply(id)
+            project.plugins.apply(id)
         } catch (e: Exception) {
             throw VitalGradlePluginException.PluginNotFound(id, exampleVersion)
         }
     }
 
-    private fun hasDependency(dependencyNotation: String) =
-        target.configurations.flatMap { it.allDependencies }
+    private fun hasDependency(project: Project, dependencyNotation: String) =
+        project.configurations.flatMap { it.allDependencies }
             .any { "${it.group}:${it.name}:${it.version}".startsWith(dependencyNotation) }
 
     companion object {
-        const val SHADOW_PLUGIN_ID = "com.github.johnrengelman.shadow"
-        const val SHADOW_PLUGIN_EXAMPLE_VERSION = "8.1.1"
-        const val VITAL_COMMANDS_DEPENDENCY = "me.vitalframework:vital-commands"
-        const val VITAL_COMMANDS_PROCESSOR_DEPENDENCY = "me.vitalframework:vital-commands-processor"
-        const val VITAL_CORE_DEPENDENCY = "me.vitalframework:vital-core"
-        const val VITAL_CORE_PROCESSOR_DEPENDENCY = "me.vitalframework:vital-core-processor"
+        const val SHADOW_PLUGIN_ID = "com.gradleup.shadow"
+        const val SHADOW_PLUGIN_EXAMPLE_VERSION = "8.3.6"
+        const val DEPENDENCY_VITAL_COMMANDS = "me.vitalframework:vital-commands"
+        const val DEPENDENCY_VITAL_COMMANDS_PROCESSOR = "me.vitalframework:vital-commands-processor"
+        const val DEPENDENCY_VITAL_CORE = "me.vitalframework:vital-core"
+        const val DEPENDENCY_VITAL_CORE_PROCESSOR = "me.vitalframework:vital-core-processor"
     }
 }
