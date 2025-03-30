e9d657cd2da1220d23492d208e635c1fc8aa168a
xRa1ny <mikeneumann2302@gmail.com>
2025-03-30 13:32:06 +0200

    fixed vital-commands-processor

--- vital-gradle-plugin/src/main/kotlin/me/vitalframework/VitalGradlePlugin.kt
+++ vital-gradle-plugin/src/main/kotlin/me/vitalframework/VitalGradlePlugin.kt
@@ -13,25 +13,26 @@ class VitalGradlePlugin : Plugin<Project> {
         target.plugins.apply("io.spring.dependency-management")
         target.plugins.apply("com.gradleup.shadow")
 
-        target.applyDependency("implementation", "me.vitalframework:vital-core:${target.getProperty("vitalVersion")}")
-        target.applyDependency(
-            "kapt", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}",
-            "annotationProcessor", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}"
-        )
-
-        if (target.hasDependency("me.vitalframework:vital-commands")) {
+        target.afterEvaluate {
+            target.applyDependency("implementation", "me.vitalframework:vital-core:${target.getProperty("vitalVersion")}")
             target.applyDependency(
-                "kapt", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}",
-                "annotationProcessor", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}"
+                "kapt", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}",
+                "annotationProcessor", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}"
             )
-        }
 
-        target.tasks.named("build") { it.dependsOn(target.tasks.named("shadowJar")) }
-        target.tasks.named("bootJar") { it.enabled = false }
-        target.tasks.named("shadowJar", ShadowJar::class.java) {
-            // service files are not automatically merged which can cause
-            it.mergeServiceFiles()
-            it.mergeGroovyExtensionModules()
+            if (target.hasDependency("me.vitalframework:vital-commands")) {
+                target.applyDependency(
+                    "kapt", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}",
+                    "annotationProcessor", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}"
+                )
+            }
+
+            target.tasks.named("build") { it.dependsOn(target.tasks.named("shadowJar")) }
+            target.tasks.named("bootJar") { it.enabled = false }
+            target.tasks.named("shadowJar", ShadowJar::class.java) {
+                // service files are not automatically merged which can cause
+                it.mergeServiceFiles()
+                it.mergeGroovyExtensionModules()
 //            TODO: currently does not work with logger factories on paper...
 //            appendTransform(it, "META-INF/spring.factories")
 //            appendTransform(it, "META-INF/spring.handlers")
@@ -39,29 +40,30 @@ class VitalGradlePlugin : Plugin<Project> {
 //            appendTransform(it, "META-INF/spring.tooling")
 //            appendTransform(it, "META-INF/web-fragment.xml")
 //            appendTransform(it, "META-INF/web-fragment.xml")
-            it.appendTransform("META-INF/spring/aot.factories")
-            it.appendTransform("META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports")
-            it.exclude(
-                "META-INF/DEPENDENCIES",
-                "META-INF/LICENSE",
-                "META-INF/LICENSE.md",
-                "META-INF/LICENSE.txt",
-                "META-INF/NOTICE",
-                "META-INF/NOTICE.md",
-                "META-INF/NOTICE.txt",
-                "META-INF/additional-spring-configuration-metadata.json",
-                "META-INF/license.txt",
-                "META-INF/notice.txt",
-                "META-INF/sisu/javax.inject.Named",
-                "META-INF/spring-configuration-metadata.json",
-                "META-INF/spring.factories",
-                "META-INF/spring.handlers",
-                "META-INF/spring.schemas",
-                "META-INF/spring.tooling",
-                "META-INF/web-fragment.xml",
-                "license.txt",
-                "notice.txt"
-            )
+                it.appendTransform("META-INF/spring/aot.factories")
+                it.appendTransform("META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports")
+                it.exclude(
+                    "META-INF/DEPENDENCIES",
+                    "META-INF/LICENSE",
+                    "META-INF/LICENSE.md",
+                    "META-INF/LICENSE.txt",
+                    "META-INF/NOTICE",
+                    "META-INF/NOTICE.md",
+                    "META-INF/NOTICE.txt",
+                    "META-INF/additional-spring-configuration-metadata.json",
+                    "META-INF/license.txt",
+                    "META-INF/notice.txt",
+                    "META-INF/sisu/javax.inject.Named",
+                    "META-INF/spring-configuration-metadata.json",
+                    "META-INF/spring.factories",
+                    "META-INF/spring.handlers",
+                    "META-INF/spring.schemas",
+                    "META-INF/spring.tooling",
+                    "META-INF/web-fragment.xml",
+                    "license.txt",
+                    "notice.txt"
+                )
+            }
         }
     }
 
