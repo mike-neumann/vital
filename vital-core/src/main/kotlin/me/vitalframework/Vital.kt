0408f028ed6d95ca124907f06fd4da891ee5abee
mike.neumann <mikeneumann2302@icloud.com>
2025-04-03 10:25:58 +0200

    now using file based banner

--- vital-core/src/main/kotlin/me/vitalframework/Vital.kt
+++ vital-core/src/main/kotlin/me/vitalframework/Vital.kt
@@ -1,14 +1,11 @@
 package me.vitalframework
 
-import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.core.env.Environment
 import org.springframework.core.env.StandardEnvironment
 import org.springframework.stereotype.Component
 import org.springframework.util.ClassUtils
-import java.io.PrintStream
 import java.util.*
 
 object Vital {
@@ -35,26 +32,7 @@ object Vital {
             .initializers({ it.beanFactory.registerSingleton("plugin", plugin) })
             // so system property resolution takes place
             .environment(StandardEnvironment())
-            .banner(VitalBanner())
             .run()
-    }
-
-    class VitalBanner : Banner {
-        override fun printBanner(environment: Environment, sourceClass: Class<*>?, out: PrintStream) {
-            out.print(
-                """
-                      .
-                     /\\ __     ___ _        _  ______  
-                    ( ( )\ \   / ( ) |_ __ _| | \ \ \ \ 
-                     \\/  \ \ / /| | __/ _` | |  \ \ \ \
-                      ,    \ V / | | || (_| | |  / / / /
-                    ========\_/==|_|\__\__,_|_|=/_/_/_/ 
-                                                        
-                    
-                                                        
-                    """.trimIndent()
-            )
-        }
     }
 
     @ConditionalOnBean(name = ["plugin"])
