15a971ca0b0bf323bb3b719883dbb749c07024f3
mike.neumann <mikeneumann2302@icloud.com>
2025-05-17 12:31:33 +0200

    part-way commit

--- vital-gradle-plugin/src/main/kotlin/me/vitalframework/VitalGradlePlugin.kt
+++ vital-gradle-plugin/src/main/kotlin/me/vitalframework/VitalGradlePlugin.kt
@@ -8,72 +8,70 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 class VitalGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         // needs to be applied before dependency resolution takes place.
-        target.plugins.apply("org.springframework.boot")
         target.plugins.apply("org.jetbrains.kotlin.plugin.spring")
         target.plugins.apply("io.spring.dependency-management")
+        target.plugins.apply("org.springframework.boot")
 
-        target.afterEvaluate {
-            target.applyDependency("implementation", "me.vitalframework:vital-core:${target.getProperty("vitalVersion")}")
+        target.applyDependency("implementation", "me.vitalframework:vital-core:${target.getProperty("vitalVersion")}")
+        target.applyDependency(
+            "kapt", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}",
+            "annotationProcessor", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}"
+        )
+
+        if (target.hasDependency("me.vitalframework:vital-commands")) {
             target.applyDependency(
-                "kapt", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}",
-                "annotationProcessor", "me.vitalframework:vital-core-processor:${target.getProperty("vitalVersion")}"
+                "kapt", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}",
+                "annotationProcessor", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}"
             )
+        }
 
-            if (target.hasDependency("me.vitalframework:vital-commands")) {
-                target.applyDependency(
-                    "kapt", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}",
-                    "annotationProcessor", "me.vitalframework:vital-commands-processor:${target.getProperty("vitalVersion")}"
-                )
-            }
+        target.tasks.named("bootJar", BootJar::class.java) { bootJar ->
+            bootJar.mainClass.set("")
+            bootJar.archiveFileName.set("${target.name}-old.jar")
+            bootJar.doLast {
+                val originalJar = bootJar.archiveFile.get().asFile
+                val unpackDir = target.layout.buildDirectory.dir(DIR_TMP_UNPACKED).get().asFile.apply {
+                    deleteRecursively()
+                    mkdirs()
+                }
+                target.copy {
+                    it.from(target.zipTree(originalJar))
+                    it.into(unpackDir)
+                }
+                val bootInfLib = unpackDir.resolve(DIR_BOOT_INF_LIB)
+                if (bootInfLib.exists()) {
+                    for (file in bootInfLib.listFiles()!!) {
+                        if (file.extension == "jar") {
+                            val jarFiles = target.zipTree(file)
+                            // kotlin and vital-loader dependencies MUST remain on the classpath, so the plugin can load
+                            if (jarFiles.any { it.isKotlinPackage() || it.isVitalLoaderPackage() }) {
+                                target.copy {
+                                    it.from(jarFiles)
+                                    it.into(unpackDir)
+                                }
 
-            target.tasks.named("bootJar", BootJar::class.java) { bootJar ->
-                bootJar.mainClass.set("")
-                bootJar.archiveFileName.set("${target.name}-old.jar")
-                bootJar.doLast {
-                    val originalJar = bootJar.archiveFile.get().asFile
-                    val unpackDir = target.layout.buildDirectory.dir(DIR_TMP_UNPACKED).get().asFile.apply {
-                        deleteRecursively()
-                        mkdirs()
-                    }
-                    target.copy {
-                        it.from(target.zipTree(originalJar))
-                        it.into(unpackDir)
-                    }
-                    val bootInfLib = unpackDir.resolve(DIR_BOOT_INF_LIB)
-                    if (bootInfLib.exists()) {
-                        for (file in bootInfLib.listFiles()!!) {
-                            if (file.extension == "jar") {
-                                val jarFiles = target.zipTree(file)
-                                // kotlin and vital-loader dependencies MUST remain on the classpath, so the plugin can load
-                                if (jarFiles.any { it.isKotlinPackage() || it.isVitalLoaderPackage() }) {
-                                    target.copy {
-                                        it.from(jarFiles)
-                                        it.into(unpackDir)
-                                    }
-
-                                    if (!jarFiles.any { it.isKotlinPackage() }) {
-                                        // we want to keep kotlin dependencies as a jar on the root
-                                        // otherwise we cannot use kotlin-reflect when our plugin tries to load
-                                        // and start up the vital instance
-                                        continue
-                                    }
+                                if (!jarFiles.any { it.isKotlinPackage() }) {
+                                    // we want to keep kotlin dependencies as a jar on the root
+                                    // otherwise we cannot use kotlin-reflect when our plugin tries to load
+                                    // and start up the vital instance
+                                    continue
                                 }
                             }
+                        }
 
-                            file.renameTo(unpackDir.resolve(file.name))
-                        }
-                        bootInfLib.deleteRecursively()
+                        file.renameTo(unpackDir.resolve(file.name))
                     }
-                    val bootInfClasses = unpackDir.resolve(DIR_BOOT_INF_CLASSES)
-                    if (bootInfClasses.exists()) {
-                        for (file in bootInfClasses.listFiles()!!) {
-                            file.renameTo(unpackDir.resolve(file.name))
-                        }
-                        bootInfClasses.deleteRecursively()
-                    }
-                    val repackedJar = target.layout.buildDirectory.file("${DIR_LIBS}${target.name}.jar").get().asFile
-                    target.ant.invokeMethod("zip", mapOf("destfile" to repackedJar, "basedir" to unpackDir))
+                    bootInfLib.deleteRecursively()
                 }
+                val bootInfClasses = unpackDir.resolve(DIR_BOOT_INF_CLASSES)
+                if (bootInfClasses.exists()) {
+                    for (file in bootInfClasses.listFiles()!!) {
+                        file.renameTo(unpackDir.resolve(file.name))
+                    }
+                    bootInfClasses.deleteRecursively()
+                }
+                val repackedJar = target.layout.buildDirectory.file("${DIR_LIBS}${target.name}.jar").get().asFile
+                target.ant.invokeMethod("zip", mapOf("destfile" to repackedJar, "basedir" to unpackDir))
             }
         }
     }
