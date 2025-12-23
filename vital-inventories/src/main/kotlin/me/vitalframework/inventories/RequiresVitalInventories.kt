b79ede31ecd5a9fb3048621cac8bf36b7e5d341d
mikeneumann <mikeneumann2302@icloud.com>
2025-12-23 19:22:07 +0100

    Add more documentation.

--- vital-inventories/src/main/kotlin/me/vitalframework/inventories/RequiresVitalInventories.kt
+++ vital-inventories/src/main/kotlin/me/vitalframework/inventories/RequiresVitalInventories.kt
@@ -1,18 +1,21 @@
 package me.vitalframework.inventories
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.stereotype.Component
 
 /**
- * Indicates that a class or function has a dependency on the `VitalInventoriesSubModule`.
+ * Convenience-annotation to mark a class to only be loaded as a bean, when the vital-inventories submodules is used.
+ * If not running with the vital-inventories submodule, the annotated bean will not be instantiated by spring.
  *
- * This annotation is used to conditionally include or enable configuration elements
- * based on the presence of the `VitalInventoriesSubModule` class in the classpath.
+ * Must be used in combination with [Component].
  *
- * The annotated element will only be processed if the `VitalInventoriesSubModule` class
- * is available at runtime. This is particularly useful for setups where certain features
- * should only be enabled when specific dependencies are present.
- *
- * This annotation can be applied to both classes and functions.
+ * ```java
+ * @RequiresVitalInventories
+ * @Component
+ * public class MyVitalInventoriesBean {
+ *   // ...
+ * }
+ * ```
  */
 @ConditionalOnClass(name = ["me.vitalframework.inventories.VitalInventoriesSubModule"])
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
